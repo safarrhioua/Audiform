1133e55f94f83660a5a0db8250a200f66cbdc565
Safarr <safarrhioua22@outlook.com>
2026-05-02 14:50:24 +0200

    Connection configured Asp.Identity added

--- Audiform/Program.cs
+++ Audiform/Program.cs
@@ -1,9 +1,16 @@
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddIdentity<IdentityUser,IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -17,7 +24,9 @@ app.UseHttpsRedirection();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
+
 
 app.MapStaticAssets();
 app.MapRazorPages()
---
+++ Infrastructure/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Infrastructure
+{
+    public class UserDto
+    {
+        public int UserId { get; set; }
+        public string name{ get; set; }
+    }
+}
